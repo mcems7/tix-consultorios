8e2f055927a6871e17244d19da48f2f188c6916c
ing.andres.jaramillo <ing.andres.jaramillo@f4f38980-714d-5389-f1c7-cb77d3594b30>
2012-09-08 15:02:48 +0000

    Actualización calculo de indice de masa corporal

--- system/application/config/autoload.php
+++ system/application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('lib_edad','lib_tiempo');
+$autoload['libraries'] = array('lib_edad','lib_tiempo','lib_ope');
 
 
 /*
