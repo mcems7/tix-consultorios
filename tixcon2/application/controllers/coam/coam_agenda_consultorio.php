18c241f7ceb1e735b3bd5a9ad3b1ddced6626d5f
ing.andres.jaramillo <ing.andres.jaramillo@f4f38980-714d-5389-f1c7-cb77d3594b30>
2013-01-11 15:34:07 +0000

    Actualización módulo de agenda de disponibilidades médicas

--- tixcon2/application/controllers/coam/coam_agenda_consultorio.php
+++ tixcon2/application/controllers/coam/coam_agenda_consultorio.php
@@ -5,13 +5,14 @@
 ###########################################################################
 */
 /**
- *OPUSLIBERTATI http://www.opuslibertati.org
- *Proyecto: SISTEMA DE INFORMACIÓN - GESTIÓN HOSPITALARIA
+ *TIX - http://www.tix.com.co
+ *Proyecto: TIX CONSULTORIOS
  *Nobre: Coam_agenda_consultorio
  *Tipo: controlador
  *Descripcion: Permite crear la agenda de los consultorios
- *Autor: Carlos Andrés Jaramillo Patiño <cjaramillo@opuslibertati.org>
- *Fecha de creación: 19 de abril de 2012
+ *Autor: Carlos Andrés Jaramillo Patiño <cajaramillo@tix.com.co>
+ *Fecha de creación: 06 de abril de 2012
+ *Última fecha modificación: 10 de enero de 2013
 */
 class Coam_agenda_consultorio extends CI_Controller
 {
@@ -19,53 +20,156 @@ class Coam_agenda_consultorio extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();			
-		$this->load->model('coam/coam_model');	 
-		$this->load->model('core/Usuario');
-		
+		$this->load->model('coam/coam_model');
+		$this->load->model('core/registro');	 		
 	}
 ///////////////////////////////////////////////////////////////////
 function index()
 {
 	redirect('coam/coam_agenda_consultorio/calendario');
 }
-
-function calendario()
+///////////////////////////////////////////////////////////////////
+function agendaConsultorio()
 {
-	//----------------------------------------------------------
 	$d = array();
-	$d['urlRegresar'] 	= site_url('core/home/index');
+	
+	//----------------------------------------------------------
+	$d['id_consultorio'] 	= $this->input->post('id_consultorio');
+	//----------------------------------------------------------
+	$d['consultorio'] = $this ->coam_model->obtenerConsultorio($d['id_consultorio']);
 	//----------------------------------------------------------
 	$prefs = array (
-'start_day' => 'moonday',
-'month_type' => 'long',
-'day_type' => 'long'
-);
+		'start_day'	=> 'monday',
+		'month_type'=> 'long',
+		'day_type'	=> 'long'
+	);
 
-$prefs['template'] = '{table_open}<table border="1" cellpadding="0" cellspacing="0" class="interna">{/table_open}
+$prefs['template'] = '{table_open}<table border="0" cellpadding="2" cellspacing="3" width="100%" class="calendario">{/table_open}
 {heading_row_start}<tr>{/heading_row_start}
 {heading_previous_cell}<td><a href="{previous_url}">&lt;&lt;</a></td>{/heading_previous_cell}
-{heading_title_cell}<td colspan="{colspan}" style="text-align:center; font-weight:bold"><strong>{heading}</strong></td>{/heading_title_cell}
+{heading_title_cell}<td colspan="{colspan}" class="mes"><strong>{heading}</strong>&nbsp;-&nbsp;'.$d['consultorio']['consultorio'].'</td>{/heading_title_cell}
 {heading_next_cell}<td><a href="{next_url}">&gt;&gt;</a></td>{/heading_next_cell}
 {heading_row_end}</tr>{/heading_row_end}
 {week_row_start}<tr>{/week_row_start}
 {week_day_cell}<td class="dia">{week_day}</td>{/week_day_cell}
 {week_row_end}</tr>{/week_row_end}
 {cal_row_start}<tr>{/cal_row_start}
-{cal_cell_start}<td>{/cal_cell_start}
-{cal_cell_content}<strong>{day}</strong><div class="contenido">{content}</div>{/cal_cell_content}
-{cal_cell_content_today}<div class="hoy_contenido"><a href="{content}">{day}</a></div>{/cal_cell_content_today}
+{cal_cell_start}<td class="celda">{/cal_cell_start}
+{cal_cell_content}<strong>{day}</strong><div class="contenido">{content}</div></strong>{/cal_cell_content}
+{cal_cell_content_today}<strong>{day}<div class="hoy_contenido">{content}</div></strong>{/cal_cell_content_today}
 {cal_cell_no_content}<strong>{day}</strong>{/cal_cell_no_content}
-{cal_cell_no_content_today}<div class="highlight">{day}</div>{/cal_cell_no_content_today}
+{cal_cell_no_content_today}<div class="hoy_no_contenido">{day}</div>{/cal_cell_no_content_today}
 {cal_cell_blank}&nbsp;{/cal_cell_blank}
 {cal_cell_end}</td>{/cal_cell_end}
 {cal_row_end}</tr>{/cal_row_end}
 {table_close}</table>{/table_close}';
 $this->load->library('calendar', $prefs);
-
+$this -> load -> view('coam/coam_calendario', $d);
+}
+///////////////////////////////////////////////////////////////////
+function calendario()
+{
+	//----------------------------------------------------------
+	$d = array();
+	$d['urlRegresar'] 	= site_url('core/home/index');
+	//----------------------------------------------------------
+	$d['consultorios'] = $this ->coam_model->obtenerConsultorios();
 	$this->load->view('core/core_inicio');
 	$this -> load -> view('coam/coam_agenda', $d);
 	$this->load->view('core/core_fin');
 	//----------------------------------------------------------	
 }
-
+///////////////////////////////////////////////////////////////////
+function agenda_dia_crear($dia,$mes,$anno,$id_consultorio,$val='v')
+{
+	//----------------------------------------------------------
+	$d = array();
+	$d['urlRegresar'] 	= site_url('coam/coam_agenda_consultorio/calendario');
+	//----------------------------------------------------------
+	$d['consultorio'] = $this ->coam_model->obtenerConsultorio($id_consultorio);
+	$d['dia'] = $dia;
+	$d['mes'] = $mes;
+	$d['anno'] = $anno;
+	$d['id_consultorio'] = $id_consultorio;
+	$d['val'] = $val;
+	$d['agenda'] = $this ->coam_model->obtener_agenda_dia($dia,$mes,$anno,$id_consultorio);
+	$this->load->view('core/core_inicio');
+	$this -> load -> view('coam/coam_agenda_dia', $d);
+	$this->load->view('core/core_fin');
+	//----------------------------------------------------------	
+}
+///////////////////////////////////////////////////////////////////
+function agenda_dia_consultar($dia,$mes,$anno,$id_consultorio)
+{
+	//----------------------------------------------------------
+	$d = array();
+	$d['urlRegresar'] 	= site_url('coam/coam_agenda_consultorio/calendario');
+	//----------------------------------------------------------
+	$d['consultorio'] = $this ->coam_model->obtenerConsultorio($id_consultorio);
+	$d['dia'] = $dia;
+	$d['mes'] = $mes;
+	$d['anno'] = $anno;
+	$d['id_consultorio'] = $id_consultorio;
+	$d['agenda'] = $this ->coam_model->obtener_agenda_dia($dia,$mes,$anno,$id_consultorio);
+	$this->load->view('core/core_inicio');
+	$this -> load -> view('coam/coam_agenda_dia_consultar',$d);
+	$this->load->view('core/core_fin');
+	//----------------------------------------------------------	
+}
+///////////////////////////////////////////////////////////////////
+function agenda_dia_eliminar($dia,$mes,$anno,$id_consultorio,$id)
+{
+	$this->coam_model->eliminar_dispo($id);
+	
+	$this->registro->agregar($this->session->userdata('id_usuario'),'coam',__CLASS__,__FUNCTION__
+		,'aplicacion',"Eliminada la asignación ".$id);
+	//----------------------------------------------------
+	$dt['mensaje']  = "La operación se realizo con exito!!";
+	$dt['urlRegresar'] 	= site_url("coam/coam_agenda_consultorio/agenda_dia_crear/$dia/$mes/$anno/$id_consultorio");
+	$this->load->view('core/presentacionMensaje', $dt);
+	return;		
+}
+///////////////////////////////////////////////////////////////////
+function agregar_dispo()
+{
+	//----------------------------------------------------------
+	$d = array();
+	//----------------------------------------------------------
+	$d['id_consultorio'] 	= $this->input->post('id_consultorio');
+	//----------------------------------------------------------	
+	$d['anno'] 	= $this->input->post('anno');
+	$d['mes'] 	= $this->input->post('mes');
+	$d['dia'] 	= $this->input->post('dia');
+	$d['hora_inicio'] 	= $this->input->post('hora_inicio');
+	$d['hora_fin'] 	= $this->input->post('hora_fin');
+	$d['min_inicio'] 	= $this->input->post('min_inicio');
+	$d['min_fin'] 	= $this->input->post('min_fin');
+	$d['tiempo_consulta'] 	= $this->input->post('tiempo_consulta');
+	$d['id_medico'] 	= $this->input->post('medico_ID');
+	$val = $this->coam_model->validar_disponibilidadDB($d);
+	if($val != 'f')
+		$this->coam_model->agregar_dispoDB($d);
+	redirect('coam/coam_agenda_consultorio/agenda_dia_crear/'.$d['dia'].'/'.$d['mes'].'/'.$d['anno'].'/'.$d['id_consultorio'].'/'.$val);
+}
+///////////////////////////////////////////////////////////////////
+function obtener_medico($l)
+{
+	$l = preg_replace("/[^a-z0-9 ]/si","",$l);
+	$this->load->database();
+	$this->db->like('core_tercero.primer_nombre',$l);
+	$this->db->or_like('core_tercero.segundo_nombre',$l);
+	$this->db->or_like('core_tercero.primer_apellido',$l);
+	$this->db->or_like('core_tercero.segundo_apellido',$l);
+	$this->db->select("
+	CONCAT(core_tercero.primer_nombre,' ',core_tercero.segundo_nombre,' ',core_tercero.primer_apellido,' ',  core_tercero.segundo_apellido) AS medico,core_medico.id_medico",FALSE);
+  $this->db->from('core_tercero');
+  $this->db->JOIN('core_medico','core_tercero.id_tercero = core_medico.id_tercero');
+	$r = $this->db->get();
+	$dat = $r -> result_array();
+	foreach($dat as $d)
+	{
+		echo $d["id_medico"]."###".$d["medico"]."|";
+	}
+}
+///////////////////////////////////////////////////////////////////
 }
--- tixcon2/application/views/coam/coam_agenda.php
+++ tixcon2/application/views/coam/coam_agenda.php
@@ -1,18 +1,82 @@
-<link rel="stylesheet" href="http://200.110.171.85/yage_sanpio/resources/styles/calendario_agenda.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?=base_url()?>resources/styles/calendario_agenda.css" type="text/css" media="screen" />
+<script language="javascript">
+////////////////////////////////////////////////////////////////////////////////
+function regresar()
+{
+	document.location = "<?php echo $urlRegresar; ?>";
+}
+////////////////////////////////////////////////////////////////////////////////
+window.addEvent("domready", function(){
+	obtenerConsultorio();					 
+});
+////////////////////////////////////////////////////////////////////////////////
+function obtenerConsultorio()
+{
+	var consultorio = $('id_consultorio').value;
+	if(consultorio == 0){
+		return false;
+	}
+	
+	var var_url = '<?=site_url()?>/coam/coam_agenda_consultorio/agendaConsultorio';
+	var ajax1 = new Request(
+	{
+		url: var_url,
+		method: 'post',
+		data:  $('formulario').toQueryString(),
+		onRequest: function (){$('div_precarga').style.display = "block";},
+		onSuccess: function(html){$('div_detalle_consultorio').set('html', html);
+		$('div_precarga').style.display = "none";},
+		onComplete: function(){},
+		evalScripts: true,
+		onFailure: function(){alert('Error ejecutando ajax!');
+		$('div_precarga').style.display = "none";}
+	});
+	ajax1.send();		
+}
+////////////////////////////////////////////////////////////////////////////////
+function crear_agenda(d,m,a,id_consultorio)
+{
+	document.location = '<?=site_url()?>/coam/coam_agenda_consultorio/agenda_dia_crear/'+d+'/'+m+'/'+a+'/'+id_consultorio;	
+}
+function consultar_agenda(d,m,a,id_consultorio)
+{
+	document.location = '<?=site_url()?>/coam/coam_agenda_consultorio/agenda_dia_consultar/'+d+'/'+m+'/'+a+'/'+id_consultorio;	
+}
+////////////////////////////////////////////////////////////////////////////////
+</script>
+<?php
+$fecha_actual = date('Y-m-d H:i:s');
+$attributes = array('id'       => 'formulario',
+	                'name'     => 'formulario',
+					'method'   => 'post');
+echo form_open('',$attributes);
+?>
+<h1 class="tituloppal">Módulo consulta ambulatoria</h1>
+<h2 class="subtitulo">Agenda de consultorio</h2>
 <center>
 <table width="100%" class="tabla_form">
-<tr><th>Agenda</th></tr>
-<tr><td>
+<tr>
+  <th style="text-align:right">
+Consultorio:&nbsp;
 <?php
-		$d = array(
-3 => 'http://your-site.com/news/article/2006/03/<br>http://your-site.com/news/article/2006/03/',
-7 => 'http://your-site.com/news/article/2006/07/',
-19 => 'http://your-site.com/news/article/2006/13/',
-26 => 'http://your-site.com/news/article/2006/26/'
-);
-	
-echo	$this->calendar->generate(2012,4,$d);
+$id = $this->session->userdata('id_consultorioCoam');
 ?>
+<select name="id_consultorio" id="id_consultorio" onchange="obtenerConsultorio()">
+    <option value="0">-Seleccione uno-</option>
+<?php
+foreach($consultorios as $d)
+{
+	if($id == $d['id_consultorio']){
+	echo '<option value="'.$d['id_consultorio'].'" selected="selected">'.$d['consultorio'].'</option>';	
+	}else{
+	echo '<option value="'.$d['id_consultorio'].'">'.$d['consultorio'].'</option>';	
+	}
+}
+?>      
+    </select>
+</th></tr>
+<tr><td id="div_detalle_consultorio">
+
 </td></tr>
 <tr><td class="linea_azul">&nbsp;</td></tr>
   <tr><td align="center">
@@ -28,3 +92,4 @@ echo form_input($data);
 </table>
 &nbsp;
 </center>
+<?=form_close();?>
